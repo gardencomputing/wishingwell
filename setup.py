68b27e44948d8a1bba27ff815ba48278a1bf7113
gardencomputing <156559084+gardencomputing@users.noreply.github.com>
2024-01-13 21:22:51 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#setup.py
+
+from distutils.core import setup
+
+setup(
+    name='Wishing-Well',
+    version='0.0.1dev',
+    packages=['Wishing Well',],
+    license="GNU Public License",
+    long_description=open('README.txt').read(),
+    )
